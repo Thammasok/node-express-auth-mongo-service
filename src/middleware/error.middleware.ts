941ba4c5b6fb2c5ca4f9d9ad24377ec891e1a473
jaranchai <jaranchai.nt@hotmail.com>
2025-04-16 23:54:43 +0700

    [Added] add error middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+import HttpException from '@/exceptions/HttpException'
+
+const errorMiddleware = (
+  error: HttpException,
+  _request: Request,
+  response: Response,
+  _next: NextFunction,
+) => {
+  const status = error.status || 500
+  const message = error.message || error.stack || 'Something went wrong'
+
+  return response.status(status).send({
+    message,
+    status,
+  })
+}
+
+export default errorMiddleware
